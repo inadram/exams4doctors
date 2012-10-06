75f6bc1ae4b4246a865d66d4397bdf568f9f93bc
Amir Mardani <inadram@gmail.com>
2012-10-06 16:25:32 -0700

    b-105 desc: adding Choice 1 and 2 to the editor in adding and updating question

--- src/Orchard.Web/Core/Common/Models/BodyPartRecord.cs
+++ src/Orchard.Web/Core/Common/Models/BodyPartRecord.cs
@@ -5,6 +5,8 @@ namespace Orchard.Core.Common.Models {
     public class BodyPartRecord : ContentPartVersionRecord {
         [StringLengthMax]
         public virtual string Text { get; set; }
+        public virtual string Choice { get; set; }
+        public virtual string Choice2 { get; set; }
 
         public virtual string Format { get; set; }
     }
--- src/Orchard.Web/Core/Common/ViewModels/BodyEditorViewModel.cs
+++ src/Orchard.Web/Core/Common/ViewModels/BodyEditorViewModel.cs
@@ -8,7 +8,17 @@ namespace Orchard.Core.Common.ViewModels {
             get { return BodyPart.Record.Text; }
             set { BodyPart.Record.Text = value; }
         }
+        public string Choice
+        {
+            get { return BodyPart.Record.Choice; }
+            set { BodyPart.Record.Choice = value; }
+        }
 
+        public string Choice2
+        {
+            get { return BodyPart.Record.Choice2; }
+            set { BodyPart.Record.Choice2 = value; }
+        }
         public string Format {
             get { return BodyPart.Record.Format; }
             set { BodyPart.Record.Format = value; }
